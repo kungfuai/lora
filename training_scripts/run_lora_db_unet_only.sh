9343ca698eb9d907b425ad1e7d3e07b98f22e82d
Edward Cates <edwardcates7@gmail.com>
2024-02-08 15:42:13 +0000

    fixing bugs to make training actually run

--- training_scripts/run_lora_db_unet_only.sh
+++ training_scripts/run_lora_db_unet_only.sh
@@ -1,17 +1,20 @@
 #https://github.com/huggingface/diffusers/tree/main/examples/dreambooth
-export MODEL_NAME="stabilityai/stable-diffusion-2-1-base"
-export INSTANCE_DIR="./data_example"
-export OUTPUT_DIR="./output_example"
+# export MODEL_NAME="stabilityai/stable-diffusion-2-1-base"
+export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export INSTANCE_DIR="../../data_copy"
+export OUTPUT_DIR="./output_examples/$(date +'%Y_%m_%d_%H_%M_%S')"
+
+mkdir -p $OUTPUT_DIR
 
 accelerate launch train_lora_dreambooth.py \
   --pretrained_model_name_or_path=$MODEL_NAME  \
   --instance_data_dir=$INSTANCE_DIR \
   --output_dir=$OUTPUT_DIR \
-  --instance_prompt="style of sks" \
+  --instance_prompt="christmas_postcard" \
   --resolution=512 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=1 \
   --learning_rate=1e-4 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --max_train_steps=30000
+  --max_train_steps=10
--- training_scripts/run_lora_db_w_text.sh
+++ training_scripts/run_lora_db_w_text.sh
@@ -1,13 +1,16 @@
 #https://github.com/huggingface/diffusers/tree/main/examples/dreambooth
-export MODEL_NAME="stabilityai/stable-diffusion-2-1-base"
-export INSTANCE_DIR="./data_example_text"
-export OUTPUT_DIR="./output_example_text"
+# export MODEL_NAME="stabilityai/stable-diffusion-2-1-base"
+export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export INSTANCE_DIR="../../data_copy"
+export OUTPUT_DIR="./output_examples/$(date +'%Y_%m_%d_%H_%M_%S')"
+
+mkdir -p $OUTPUT_DIR
 
 accelerate launch train_lora_dreambooth.py \
   --pretrained_model_name_or_path=$MODEL_NAME  \
   --instance_data_dir=$INSTANCE_DIR \
   --output_dir=$OUTPUT_DIR \
-  --instance_prompt="game character bnha" \
+  --instance_prompt="christmas_postcard" \
   --train_text_encoder \
   --resolution=512 \
   --train_batch_size=1 \
